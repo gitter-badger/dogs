74bb1bb83ccee237a68c16fee56da4d20108963e
Nicolas A Perez <nperez@ipcoop.com>
2016-02-01 09:44:56 -0500

    Starting Binomial Queue

--- core/src/main/scala/dogs/PriorityQueue.scala
+++ core/src/main/scala/dogs/PriorityQueue.scala
@@ -11,4 +11,30 @@ trait PriorityQueue[A] {
   def remove(x: A): PriorityQueue[A]
 
   def isEmpty: Boolean
+  def empty(): PriorityQueue[A]
 }
+
+case object EmptyQueue extends PriorityQueue[Nothing] {
+  override def findMin(): Nothing = this
+
+  override def empty(): PriorityQueue[Nothing] = ???
+
+  override def remove(x: Nothing): PriorityQueue[Nothing] = ???
+
+  override def isEmpty: Boolean = ???
+
+  override def add(x: Nothing): PriorityQueue[Nothing] = ???
+}
+
+class BinomialQueue[A] extends PriorityQueue[A] {
+  override def findMin(): A = ???
+
+  override def empty(): PriorityQueue[A] = ???
+
+  override def remove(x: A): PriorityQueue[A] = ???
+
+  override def isEmpty: Boolean = ???
+
+  override def add(x: A): PriorityQueue[A] = ???
+}
+
